962aaeb217e9d357defd9d27e4238d4cef581382
RavenCha <113488948+RavenCha@users.noreply.github.com>
2022-10-26 14:53:10 +0800

    Add files via upload

---
+++ ENGAGING ACTIVITY/ENGAGING ACTIVITY/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Raven
+ * Date: 10/26/2022
+ * Time: 12:10 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+namespace ENGAGING_ACTIVITY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Stack st = new Stack();
+            st.Push("E");
+            st.Push("V");
+            st.Push("A");
+            st.Push("R");
+            Console.WriteLine("Current stack: ");
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            st.Push("C");
+            st.Push("N");
+            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+            Console.WriteLine("Current stack: ");
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Removing values");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("Current stack: ");
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
