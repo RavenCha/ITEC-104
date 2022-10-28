3e617ad3fd89f363b2be850ad8131eb74a2e4e05
RavenCha <113488948+RavenCha@users.noreply.github.com>
2022-10-28 23:46:01 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY5_CRUZ/ITEC104_ACTIVITY5_CRUZ/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Raven
+ * Date: 10/27/2022
+ * Time: 2:29 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ITEC104_ACTIVITY5_CRUZ
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+           static void Main(string[] args)
+        {            
+           List<int> mylist = test(new List<int>(new int[] { 10, 20, 35 , 65, 53, 48, 5, 1}));                  
+           foreach(var i in mylist)
+            {
+               Console.Write(i.ToString()+" ");
+            }     
+        }                  
+        public static List<int> test(List<int> nums)
+          {
+             return nums.Where(n => n % 10 < 5).ToList();
+          }    
+    }
+}
